15dda290ea60cd954b06a736a9458187f213bc2c
rajuh301 <rajuh301@gmail.com>
2023-03-17 18:46:09 +0600

    js basic

---
+++ jaan-2.js
@@ -0,0 +1,10 @@
+var macPrice = 80000;
+var gamingLaptop = 60000;
+var lenovoYoga = 40000;
+var usedLaptop = 20000;
+
+
+var myTk = 5000;
+
+
+if()
---
+++ jaan.js
@@ -0,0 +1,117 @@
+/*
+basic--------------
+
+1. What is javaScript? 
+ans: javaScript is a Scripting language. 
+
+2.  How does javaScript work?
+ans: JavaScript work with html and css to build web app or web pages.
+
+3. What is Variable?
+ans: Variables are use to stor data.
+
+4. How to declare a variable?
+ans: var product = "soap";
+
+5. Type of variable?
+ans:There are three types of variables. 
+For example: i) number  ii) string  iii) boolean
+
+-- How can you find out type of a variable? 
+ans: var friend ='islam';
+     console.log(typeof(friend));
+
+6. Primitive and non-primitive data types!
+
+and: primitive data types: There are seven primitive data type. 
+    For example: 1) Number  2) String   3) Boolean  4) Undefined    
+    5) Null      6) bigint   7) Symbol
+
+    non-primitive data type: 1) object
+
+7. Naming Convention of javaScript variable.
+ans: For create a variable should knows five things.
+    For example: 1) var  2) name   3) =  4) data   5) ; 
+    more example: var productPrice = 40;
+
+8. Math Operation +, -, *, /, %
+
+------------------------ for + -------------
+var applePrice = 450;
+var bananaPrice = 200;
+console.log(applePrice + bananaPrice);
+
+------------------------ for - -------------
+var applePrice = 450;
+var bananaPrice = 200;
+console.log(applePrice - bananaPrice);
+
+------------------------ for * -------------
+var appleWhite = 450;
+var applePriceKg= 10;
+console.log(applePrice * bananaPrice);
+
+------------------------ for / -------------
+var banana = 10;
+var person = 5;
+console.log(banana / person);
+
+------------------------ for % -------------
+var banana = 11;
+var person = 5;
+console.log(banana % person);
+
+
+9. Short hand: 
+ans:    += increase,     -= decrease, *= multiple,  /= divided
+ 
+10. what is ++ or -- ?
+ans: ++ increase one, -- decrease one 
+
+11. what is ParseInt and ParseFloat?
+ans: ParseInt for number value, and ParseFloat for decimal number,
+    this method are work for convert data type.
+
+12. what is toFixed ?
+ans: 
+     var banana = 14;
+     var person = 3;
+     var giveBanana = banana / person;
+     console.log(giveBanana.toFixed(2));
+   
+
+
+----------------------------- ARRAY --------------------------
+
+1. What is the purpose of array?
+ans: array use for store multiple data.
+
+2. How to declare an array in js?
+ans: var phoneBrand=['Samsung', 'Nokia', 'Iphone'];
+
+3. Number of elements in a array.
+ans: 
+    var phoneBrand=['Samsung', 'Nokia', 'Iphone'];
+    console.log(phoneBrand.length);
+
+4. What is index ?
+ans: Index is a value position of array. 
+
+5. Find the value of an element by index.
+ans:
+    var phoneBrand=['Samsung', 'Nokia', 'Iphone'];
+    console.log(phoneBrand[1]);
+
+6. How to change the element by index? 
+ans:
+    var phoneBrand=['Samsung', 'Nokia', 'Iphone'];
+    phoneBrand[1]='Vivo'
+    console.log(phoneBrand);
+
+
+
+*/
+
+
+
+
